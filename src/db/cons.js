74e5dfeeeccd4af64d103bd3cf082003c36bfe3e
Jay Suresh Bhakte <70802432+jaybhakte@users.noreply.github.com>
2022-11-19 18:30:25 +0530

    second commit

--- src/db/cons.js
+++ src/db/cons.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost:27017/roomysRegistration").then(()=>{
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/roomysRegistration",{ useNewUrlParser: true }).then(()=>{
     console.log("Connection succeful");
 }).catch((e)=>{
     console.log(e,"No Connection");
