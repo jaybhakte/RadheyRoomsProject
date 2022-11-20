a1597e70972e7b7a036e6c84f507df0290bc5d8a
Jay Suresh Bhakte <70802432+jaybhakte@users.noreply.github.com>
2022-11-20 17:23:07 +0530

    new changes db

--- src/db/cons.js
+++ src/db/cons.js
@@ -1,8 +1,13 @@
 const mongoose = require('mongoose');
+const uri = process.env.MONGODB_URI;
+// process.env.config_variable_name
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/roomysRegistration",{ useNewUrlParser: true }).then(()=>{
+//     console.log("Connection succeful");
+// }).catch((e)=>{
+//     console.log(e,"No Connection");
+// })
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/roomysRegistration",{ useNewUrlParser: true }).then(()=>{
-    console.log("Connection succeful");
-}).catch((e)=>{
-    console.log(e,"No Connection");
-})
-
+mongoose
+.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true })
+.then(() => console.log('MongoDB connected!'))
+.catch(err => console.log('Error:- ' + err));
